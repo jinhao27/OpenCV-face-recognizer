d3054ed6b937fc60c95443ec516a37381a9e6637
Jinhao <53026474+jinhao27@users.noreply.github.com>
2020-03-15 01:51:53 -0400

    First commit

---
+++ test.py
@@ -0,0 +1,53 @@
+import numpy as np
+import cv2
+import pickle
+
+face_cascade = cv2.CascadeClassifier(r'cv\data\haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainner.yml")
+
+labels = {"person_name": 1}
+with open("cv/labels.pickle", 'rb') as f:
+	og_labels = pickle.load(f)
+	labels = {v:k for k,v in og_labels.items()}
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        # print(x,y,w,h)
+        roi_gray = gray[y:y+h, x:x+w]
+        # recognized
+        id_, conf = recognizer.predict(roi_gray)
+        if conf >= 45:
+            print(id_)
+            print(labels[id_])
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = labels[id_]
+            color = (255, 255, 255)
+            stroke = 2
+            cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+        img_item = "my-image2.png"
+        cv2.imwrite(img_item, roi_gray)
+
+        color = (255, 0, 0)
+        stroke = 2
+        end_coord_x = x + w
+        end_coord_y = y + h
+        cv2.rectangle(frame, (x, y), (end_coord_x, end_coord_y), color, stroke)
+
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    cv2.imshow('gray',gray)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
---
+++ testt.py
@@ -0,0 +1,3 @@
+i = ['horses', 'pigs', 'cows']
+for (x, y, h, w) in i:
+    print(j)
